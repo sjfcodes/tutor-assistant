4a547b3283bb3fe61971338f893ffaefc2a74c1f
Samuel Fox <samueljasonfox@gmail.com>
2021-09-23 10:53:25 -0700

    update: authorization function name

--- utils/auth.js
+++ utils/auth.js
@@ -4,7 +4,7 @@ const secret = process.env.SECRET;
 const expiration = '2h';
 
 module.exports = {
-  authMiddleware: function ({ req }) {
+  authorizeTutor: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
     if (req.headers.authorization) token = token.split(' ').pop().trim();
     if (!token) return req;
