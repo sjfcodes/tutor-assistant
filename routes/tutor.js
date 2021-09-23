98953a479852126d62f66cf4ca9c579ebf10dd3a
Samuel Fox <samueljasonfox@gmail.com>
2021-09-23 11:35:29 -0700

    update: routes are functioning to create a new tutor, login as a tutor, and add a new student to a tutor

--- routes/tutor.js
+++ routes/tutor.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { Tutor, Student } = require("../models");
-const { signToken } = require('../utils/auth');
+const { signToken, authorizeTutor } = require('../utils/auth');
 
 
 router.post("/", async ({ body }, res) => {
@@ -13,22 +13,26 @@ router.post("/login", async ({ body }, res) => {
     const tutor = await Tutor.findOne({ email: body.email });
     if (!tutor) return res.status(401).json('No tutor found with this email address');
 
-    const correctPw = await Tutor.isCorrectPassword(body.password);
+    const correctPw = await tutor.isCorrectPassword(body.password);
     if (!correctPw) return res.status(401).json('Incorrect credentials');
 
     const token = signToken(tutor);
     res.json({ token, tutor });
 });
 
-router.post("/student", async ({ body }, res) => {
+router.post("/student", async (req, res) => {
+    const { tutor } = authorizeTutor(req)
+    if (!tutor) return res.status(401).json('unauthorized')
 
-    const student = await Student.create(body);
+    const student = await Student.create(req.body);
     if (!student) return res.status(500).json('failed to create student')
 
-    const tutor = await Tutor.findOneAndUpdate({ _id: body.user_id }, { $addToSet: { students: student._id } });
-    if (!tutor) return res.status(401).json('failed to add student to tutor');
+    const updatedTutor = await Tutor.findOneAndUpdate({ _id: tutor._id }, { $addToSet: { students: student._id } });
+    if (!updatedTutor) return res.status(500).json('failed to add student to tutor');
 
-    res.json('ok')
+    res.json('student added')
 })
+
+// router.get('/student/:id',)
 
 module.exports = router;
