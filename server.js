160605ac5e756cbd8b539c5fa50cb65e1832c005
Samuel Fox <samueljasonfox@gmail.com>
2021-09-24 10:35:25 -0700

    add: cors package

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const cors = require('cors')
 require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', require('./routes'))
