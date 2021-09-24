7bcdbae557ec462b6e36c0e92dde3ebbe3a7a11a
Samuel Fox <samueljasonfox@gmail.com>
2021-09-23 17:15:09 -0700

    add: module for session route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const app = require('express').Router()
 const tutorRoutes = require('./tutor')
+const studentRoutes = require('./student')
+const sessionRoutes = require('./session')
 
 app.use('/tutor', tutorRoutes)
+app.use('/student', studentRoutes)
+app.use('/session', sessionRoutes)
 
 module.exports = app
---
+++ routes/session.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const { Session } = require('../models')
+
+router.put('./session/:id', async (req, res) => {
+    const { tutor } = authorizeTutor(req);
+    if (!tutor) return res.status(401).json('unauthorized');
+
+    const updated = await Session.findOneAndUpdate(req.params.id, req.body)
+    if (!updated) res.status(500).json('failed to update')
+
+    res.json('session updated')
+
+})
+
+router.delete('/session/:id', async (req, res) => {
+    const { tutor } = authorizeTutor(req);
+    if (!tutor) return res.status(401).json('unauthorized');
+
+    const deleted = await Session.findOneAndDelete(req.params.id)
+    if (!deleted) res.status(500).json('failed to delete')
+
+    res.json('session deleted')
+})
+
+module.exports = router
