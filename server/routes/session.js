9086d8db98cc739826e1daac188ca37e56256ba0
Samuel Fox <samueljasonfox@gmail.com>
2021-09-28 12:05:58 -0700

    - update: route to edit a session - add: route to delete a session

--- server/routes/session.js
+++ server/routes/session.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const { Session } = require('../models');
 const { authorizeToken } = require('../utils/auth');
-const { addModelToTutor } = require('../utils/helpers');
+const { addModelToTutor, deleteModelFromTutor } = require('../utils/helpers');
 
 
 router.post('/', async (req, res) => {
@@ -12,34 +12,46 @@ router.post('/', async (req, res) => {
     if (!session) return res.statusMessage(500).json('failed to create session');
 
     try {
-        await addModelToTutor(tutor._id, 'sessions', session._id)
+        await addModelToTutor(tutor._id, 'sessions', session._id);
         res.json('session added');
 
     } catch (error) {
-        console.log(error)
-        res.status(500).json('failed to update tutor')
-    }
-})
+        console.log(error);
+        res.status(500).json('failed to update tutor');
+    };
+});
 
-router.put('./session/:id', async (req, res) => {
+// update a sessions information
+router.put('/', async (req, res) => {
     const { tutor } = authorizeToken(req);
     if (!tutor) return res.status(401).json('unauthorized');
 
-    const updated = await Session.findOneAndUpdate(req.params.id, req.body)
-    if (!updated) res.status(500).json('failed to update')
+    try {
+        const session = await Session.findByIdAndUpdate(req.body._id, req.body);
+        if (!session) return res.status(404).json('session not found');
 
-    res.json('session updated')
+        res.json('session updated');
 
-})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json('failed to update session');
+    };
+});
 
-router.delete('/session/:id', async (req, res) => {
+// delete a session and remove the reference from the tutor
+router.delete('/', async (req, res) => {
     const { tutor } = authorizeToken(req);
     if (!tutor) return res.status(401).json('unauthorized');
 
-    const deleted = await Session.findOneAndDelete(req.params.id)
-    if (!deleted) res.status(500).json('failed to delete')
+    try {
+        await deleteModelFromTutor(tutor._id, "sessions", Session, req.body._id);
+        res.json('session deleted');
 
-    res.json('session deleted')
-})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json('failed to delete session');
+    };
+
+});
 
 module.exports = router
