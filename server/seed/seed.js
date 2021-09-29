c4a4c74c77dcb473b92bea830b4f14308965e805
Samuel Fox <samueljasonfox@gmail.com>
2021-09-28 17:20:55 -0700

    add: template seeding

---
+++ server/seed/seed.js
@@ -0,0 +1,13 @@
+const db = require('../config/connection');
+const { Template } = require('../models')
+const templateSeeds = require('./templateSeeds.json')
+
+db.once('open', async () => {
+    try {
+        await Template.deleteMany({});
+        await Template.create(templateSeeds)
+    } catch (error) {
+        console.log(error)
+    }
+    process.exit(0);
+})
