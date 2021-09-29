92eff2ada944c9d41de663d71b1318ac23c52861
Samuel Fox <samueljasonfox@gmail.com>
2021-09-28 17:20:09 -0700

    add: template routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,11 @@ const app = require('express').Router()
 const tutorRoutes = require('./tutor')
 const studentRoutes = require('./student')
 const sessionRoutes = require('./session')
+const templateRoutes = require('./template')
 
 app.use('/tutor', tutorRoutes)
 app.use('/student', studentRoutes)
 app.use('/session', sessionRoutes)
+app.use('/template', templateRoutes)
 
 module.exports = app
