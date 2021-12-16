8a21a7f315c8503a615c426b5342d5addda5c213
Samuel Fox <samueljasonfox@gmail.com>
2021-12-16 01:25:16 -0800

    attempt deployment

--- server/server.js
+++ server/server.js
@@ -13,10 +13,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', require('./routes'));
 
-app.use(express.static(path.join(__dirname, '../build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.listen(PORT, () => {
