bc0a6835c8e1125645c1ee69b6d2648047c50e47
Samuel Fox <samueljasonfox@gmail.com>
2021-12-17 17:14:25 -0800

    debug path to build folder

--- server/server.js
+++ server/server.js
@@ -12,11 +12,15 @@ app.use(cors());
 app.use(compression());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/', require('./routes'));
+app.use(require('./routes'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}!`);
