f75b7d1546b5323cc868504d308021455ffdf11c
Samuel Fox <samueljasonfox@gmail.com>
2021-12-29 12:14:17 -0800

    turn off plural for calendly model name

--- server/models/Calendly.js
+++ server/models/Calendly.js
@@ -1,17 +1,22 @@
 const { Schema, model } = require('mongoose');
 
-const calendlySchema = new Schema({
-  avatar_url: { type: String },
-  created_at: { type: String },
-  current_organization: { type: String },
-  email: { type: String },
-  name: { type: String },
-  scheduling_url: { type: String },
-  slug: { type: String },
-  timezone: { type: String },
-  updated_at: { type: String },
-  uri: { type: String },
-});
+const calendlySchema = new Schema(
+  {
+    avatar_url: { type: String },
+    created_at: { type: String },
+    current_organization: { type: String },
+    email: { type: String },
+    name: { type: String },
+    scheduling_url: { type: String },
+    slug: { type: String },
+    timezone: { type: String },
+    updated_at: { type: String },
+    uri: { type: String },
+  },
+  {
+    collection: 'Calendly',
+  },
+);
 
 const Calendly = model('Calendly', calendlySchema);
 
