ec076ce973294c95c094d59f24402c3523c7b9ac
Samuel Fox <samueljasonfox@gmail.com>
2021-12-29 11:59:34 -0800

    debugging deployment calendly route

--- server/routes/api/calendly.js
+++ server/routes/api/calendly.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const axios = require('axios');
-const { Tutor, Calendly } = require('../../models');
+// const { Tutor, Calendly } = require('../../models');
 const { authorizeToken } = require('../../utils/auth');
 const { getCalendlyEvents, getCalendlyHeaders } = require('../../utils/calendly-helpers');
 const { getCalendlyToken } = require('../../utils/encryption');
@@ -19,19 +19,17 @@ router.post('/users/me', authorizeToken, async (req, res) => {
     const url = 'https://api.calendly.com/users/me';
     const options = { headers: getCalendlyHeaders(decryptedToken) };
     const { data: { resource } } = await axios.get(url, options);
-    // if resource is object is empty
-    if (!Object.keys(resource).length) return res.status(500).json('failed:2');
+    return res.json({ test: resource });
     // update tutors details
 
-    const { _id } = await Calendly.create(resource).catch((error) => res.status(500).json(error));
-    await Tutor.findByIdAndUpdate(
-      req.tutor._id,
-      { calendly: _id },
-      { new: true },
-    ).catch((error) => res.status(500).json(error));
-    // send resource to client to update local state
-    req.body.uri = resource.uri;
-    return res.json({ resource });
+    // const { _id } = await Calendly.create(resource);
+    // await Tutor.findByIdAndUpdate(
+    //   req.tutor._id,
+    //   { calendly: _id },
+    //   { new: true },
+    // ).catch((error) => res.status(500).json(error));
+    // // send resource to client to update local state
+    // return res.json({ resource });
   } catch (error) {
     console.error(error);
     return res.status(500).json('failed:3');
