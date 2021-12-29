6c4445e58e8d92373a8b41e147699bb57c34c820
Samuel Fox <samueljasonfox@gmail.com>
2021-12-29 14:43:00 -0800

    remove accesscode seed, update signup route

--- server/seed/index.js
+++ server/seed/index.js
@@ -11,7 +11,7 @@ const emailTemplateSeeds = require('./emailTemplate.json');
 const courseSeeds = require('./course.json');
 const studentSeeds = require('./student');
 const meetingSeeds = require('./meeting');
-const accessTokenSeeds = require('./accesstoken.json');
+// const accessTokenSeeds = require('./accesstoken.json');
 
 db.once('open', async () => {
   try {
@@ -30,7 +30,7 @@ db.once('open', async () => {
     await Student.create(studentSeeds);
     await Meeting.create(meetingSeeds);
     await EmailTemplate.create(emailTemplateSeeds);
-    await AccessToken.create(accessTokenSeeds);
+    // await AccessToken.create(accessTokenSeeds);
 
     console.log('==> db seeds success\n');
   } catch (error) {
