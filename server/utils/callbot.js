670f54469694e481b675feaf767e376f9b21b711
Samuel Fox <samueljasonfox@gmail.com>
2022-03-03 08:43:23 -0800

    update callbot

--- server/utils/callbot.js
+++ server/utils/callbot.js
@@ -1,8 +1,11 @@
-const axios = require('axios');
+/* eslint-disable no-console */
 /*
 this bot is not necessarily part of this project,
 simply a tool keep the email server and some other project servers from sleeping
 */
+const axios = require('axios');
+
+const minute = 60 * 1000;
 const endPoints = [
   'https://radcats-karaoke.herokuapp.com/',
   'https://sjf-react-email-templates.herokuapp.com/',
@@ -18,6 +21,5 @@ const refreshOtherServers = async (urls) => {
   }
 };
 
-const minute = 60 * 1000;
-
+refreshOtherServers(endPoints);
 setInterval(() => refreshOtherServers(endPoints), minute * 20);
